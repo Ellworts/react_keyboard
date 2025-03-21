df9e369b6d3c96f3f931edd42465d44cf1aa2e25
Misha Kuptsov <azazin1955@gmail.com>
2025-03-21 15:12:20 +0000

    ternary operator is neatly formatted

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,11 @@ export class App extends React.Component {
     return (
       <div className="App">
         <p className="App__message">
-          {this.state.pressedKey
-            ? `The last pressed key is [${this.state.pressedKey}]`
-            : `Nothing was pressed yet`}
+          {this.state.pressedKey ? (
+            `The last pressed key is [${this.state.pressedKey}]`
+          ) : (
+            `Nothing was pressed yet`
+          )}
         </p>
       </div>
     );
